139f6f1938ac36106c3258088a4a8b1ec1bb6e4c
Kira7124 <afc2015@naver.com>
2024-01-25 15:58:44 +0900

    1234

---
+++ src/main/java/com/tenco/bank/repository/entity/History.java
@@ -0,0 +1,29 @@
+package com.tenco.bank.repository.entity;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class History {
+	
+	private Integer id;
+	private Long amount;
+	private Long wbalance;
+	private Long dbalance;
+	private Integer wAccountId;
+	private Integer dAccountId;
+	private Timestamp createdAt;
+	
+
+	
+}
---
+++ src/main/java/com/tenco/bank/repository/entity/User.java
@@ -0,0 +1,27 @@
+package com.tenco.bank.repository.entity;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+	private Integer id;
+	private String username;
+	private String password;
+	private String fullname;
+	private Timestamp createdAt;
+	
+	
+	
+	
+}
