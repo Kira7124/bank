317576bf077d0077a24420be2d181fe979a403f1
Kira7124 <afc2015@naver.com>
2024-01-25 11:51:01 +0900

    init project

---
+++ src/test/java/com/tenco/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenco.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
