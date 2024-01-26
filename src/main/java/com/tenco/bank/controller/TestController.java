3b0b43d078f5f2f0b45e71b4b589dc0a25845c17
Kira7124 <afc2015@naver.com>
2024-01-26 14:04:52 +0900

    예외처리완료

--- src/main/java/com/tenco/bank/controller/TestController.java
+++ src/main/java/com/tenco/bank/controller/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.tenco.bank.handler.exception.CustomPageException;
 import com.tenco.bank.handler.exception.CustomRestfulException;
 
 
